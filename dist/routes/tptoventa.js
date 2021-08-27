40951cf2e794c893a2eacbaf2e9bc9dc2243ef88
rsalvarez <rafaelrio4@gmail.com>
2021-08-27 20:02:41 -0300

    Commit sacamos dist

--- dist/routes/tptoventa.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const tptoventa_1 = require("../controllers/tptoventa");
-const router = express_1.Router();
-router.get('/', tptoventa_1.getAllPtoVenta);
-router.get('/:id', tptoventa_1.getPtoventa);
-exports.default = router;
-//# sourceMappingURL=tptoventa.js.map
